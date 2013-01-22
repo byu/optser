a8ed405fa70783c2de74724144a4ece23256ba4e
Benjamin Yu <benjaminlyu@gmail.com>
2013-01-21 21:17:46 -0800

    Move version into Opster::VERSION.
    Details:
    * Optser::Version::STRING to Optser::VERSION to make it canonically
      follow how `bundle gem` creates the versioning.

--- lib/optser/version.rb
+++ lib/optser/version.rb
@@ -1,11 +1,3 @@
 module Optser
-
-  module Version
-    MAJOR = 0
-    MINOR = 1
-    PATCH = 0
-    BUILD = nil
-    STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join '.'
-  end
-
+  VERSION = '0.1.0'
 end
