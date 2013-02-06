55c703c8a879501182381ad9ac7d8b0364dc002a
Benjamin Yu <benjaminlyu@gmail.com>
2013-02-06 15:20:37 -0800

    Adds parse_options and extract_options methods.
    Details:
    * Adds parse_options to just handle the creation of an OptSet from a hash.
    * Adds extract_options (without a bang) to parse options from the
      method arguments list without popping off the options hash.

--- lib/optser.rb
+++ lib/optser.rb
@@ -22,11 +22,28 @@ require 'optser/opt_set'
 module Optser
 
   ##
+  # Parses the options from the arguments list, and creates a
+  # brand new OptSet instance to lookup the options; but does not
+  # modify the args.
+  #
+  def self.extract_options(args)
+    return parse_options(args.last.is_a?(::Hash) ? args.last : nil)
+  end
+
+  ##
   # Extracts the options from the arguments list, and creates a
-  # brand new OptSet instance to lookup the options.
+  # brand new OptSet instance to lookup the options; pops the options
+  # off the args list if we have it.
   #
   def self.extract_options!(args)
-    return Optser::OptSet.new(args.last.is_a?(::Hash) ? args.pop : nil)
+    return parse_options(args.last.is_a?(::Hash) ? args.pop : nil)
+  end
+
+  ##
+  # Creates a new OptSet from the given options.
+  #
+  def self.parse_options(options)
+    return Optser::OptSet.new options
   end
 
 end
